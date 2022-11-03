04e01e2deae41b6fff76ead566cc9594de884496
root <heribudiyana@gmail.com>
2022-11-04 00:26:10 +0700

    fitur forum

--- features/login/services/logic.go
+++ features/login/services/logic.go
@@ -5,6 +5,7 @@ import (
 	"be12/mentutor/middlewares"
 	"errors"
 	"strings"
+	"unicode"
 
 	"github.com/labstack/gommon/log"
 	"golang.org/x/crypto/bcrypt"
@@ -25,43 +26,43 @@ func (usecase *authUsecase) Login(input login.Core) (login.Core, string, error) 
 	// PENGECEKAN STRING EMAIL & PASSWORD
 
 	// Cek email mengandung spasi
-	// for _, v := range input.Email{
-	// 	if unicode.IsSpace(v) {
-	// 		return login.Core{}, "", errors.New("contain space")
-	// 	}
-	// }
-	// // Cek ketentuan email
-	// if len(input.Email) < 10 || len(input.Email) > 75  {
-	// 	return login.Core{}, "", errors.New("length not valid")
-	// } else if strings.Contains(input.Email, "@") == false && strings.Contains(input.Email, ".") == false {
-	// 	return login.Core{}, "", errors.New("not contain (@) or (.)")
-	// }
+	for _, v := range input.Email {
+		if unicode.IsSpace(v) {
+			return login.Core{}, "", errors.New("contain space")
+		}
+	}
+	// Cek ketentuan email
+	if len(input.Email) < 10 || len(input.Email) > 75 {
+		return login.Core{}, "", errors.New("length not valid")
+	} else if strings.Contains(input.Email, "@") == false && strings.Contains(input.Email, ".") == false {
+		return login.Core{}, "", errors.New("not contain (@) or (.)")
+	}
 
-	// // Cek ketentuan password
-	// var upper, lower, number, sChar int
-	// for _, v := range input.Password {
-	// 	if unicode.IsUpper(v) == true {
-	// 		upper+=1
-	// 	} else if unicode.IsLower(v) ==  true {
-	// 		lower += 1
-	// 	} else if unicode.IsNumber(v) == true {
-	// 		number+=1
-	// 	} else{
-	// 		sChar+=1
-	// 	}
-	// }
+	// Cek ketentuan password
+	var upper, lower, number, sChar int
+	for _, v := range input.Password {
+		if unicode.IsUpper(v) == true {
+			upper += 1
+		} else if unicode.IsLower(v) == true {
+			lower += 1
+		} else if unicode.IsNumber(v) == true {
+			number += 1
+		} else {
+			sChar += 1
+		}
+	}
 
-	// if upper < 1  {
-	// 	return login.Core{}, "", errors.New("string not as expected")
-	// } else if lower < 1 {
-	// 	return login.Core{}, "", errors.New("string not as expected")
-	// } else if number < 1 {
-	// 	return login.Core{}, "", errors.New("string not as expected")
-	// } else if sChar < 1 {
-	// 	return login.Core{}, "", errors.New("string not as expected")
-	// } else if len(input.Password) < 8 || len(input.Password) > 30 {
-	// 	return login.Core{}, "", errors.New("string too short or too long")
-	// }
+	if upper < 1 {
+		return login.Core{}, "", errors.New("string not as expected")
+	} else if lower < 1 {
+		return login.Core{}, "", errors.New("string not as expected")
+	} else if number < 1 {
+		return login.Core{}, "", errors.New("string not as expected")
+	} else if sChar < 1 {
+		return login.Core{}, "", errors.New("string not as expected")
+	} else if len(input.Password) < 8 || len(input.Password) > 30 {
+		return login.Core{}, "", errors.New("string too short or too long")
+	}
 
 	// Check Username di database
 	res, err := usecase.authData.Login(input)
