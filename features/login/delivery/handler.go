a6a33b0f790d0d02a274b37ce8495b9d2232bb6c
faturfawkes <nur.faturohman28@gmail.com>
2022-11-13 01:03:22 +0700

    fix bug get token google

--- features/login/delivery/handler.go
+++ features/login/delivery/handler.go
@@ -48,13 +48,18 @@ func (h *AuthDelivery) Login() echo.HandlerFunc {
 		if res.ID < 1000 {
 			err := h.authUsecase.GetToken(res.ID)
 			if err != nil {
-			url, err = helper.GetUrl()
-			// log.Print(url)
+				url, err = helper.GetUrl()
+				// log.Print(url)
 				if err != nil {
+					log.Print(err)
 					return c.JSON(http.StatusBadRequest, helper.FailedResponse("Failed get url Oauth"))
 				}
+				err = h.authUsecase.InsertToken(res.ID)
+				if err != nil {
+					log.Print(err)
+					return c.JSON(http.StatusBadRequest, helper.FailedResponse("Failed insert to db"))
+				}
 			}
-			err = h.authUsecase.InsertToken(res.ID)
 		}	
 		res.Token = token
 		return c.JSON(http.StatusOK, helper.SuccessResponse("login successful", ToResponse(res, "login", url)))
